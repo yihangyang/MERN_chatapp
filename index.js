4b0d9c5e0003875958265388efe16544779dd0fc
yihangyang <yangyihang@outlook.de>
2020-09-20 20:28:03 +0200

    auth completed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { mongodb_name, mongodb_pass } = require("./private/access");
-const { request } = require("express");
+const { request, response } = require("express");
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser')
 
 const { User } = require('./models/users');
 const { mongoURI } = require("./config/key");
+const { auth } = require("./middelwares/auth");
 
 const app = express()
 
@@ -25,6 +26,16 @@ app.use(bodyParser.urlencoded({ extended: true })) // no duplication waning
 app.use(bodyParser.json())
 app.use(cookieParser())
 
+app.get('/api/users/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id: req.id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role
+  })
+})
 app.post('/api/users/register', (req, res) => {
   const user = new User(req.body)
   user.save((err, doc) => {
@@ -33,6 +44,33 @@ app.post('/api/users/register', (req, res) => {
       success: true,
       userData: doc
     })
+  })
+})
+
+app.post('/api/users/login', (req, res) => {
+  // find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user){ 
+      return response.json({
+        loginSuccess: false,
+        message: 'Auth failed due to unvailded email'
+      })}
+      // compare the password
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch) {
+          return res.json({ loginSuccess: false, message: 'password false' })
+        }
+      })
+      // generate token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err)
+        res.cookie("abc_auth", user.token)
+          .status(200)
+          .json({
+            loginSuccess: true,
+          })
+      })
+    
   })
 })
 
